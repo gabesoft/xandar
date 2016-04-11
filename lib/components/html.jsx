a57c251428c520acfb05a92af21e9497dd5b91a3
Gabriel Adomnicai <gabesoft@gmail.com>
2016-04-10 19:17:45 -0700

    Added favicon

--- lib/components/html.jsx
+++ lib/components/html.jsx
@@ -12,13 +12,17 @@ module.exports = React.createClass({
     return (
       <html>
         <head>
-          <meta charSet="utf-8"/>
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
-          <meta name="viewport" content="width=device-width, initial-scale=1"/>
+          <meta charSet="utf-8"/ >
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <title>{this.props.title}</title>
-          <link rel="stylesheet" href={url.resolve(assets, 'app.css')}/>
 
-          <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic|Alegreya:400,400italic,700,700italic" rel="stylesheet" type="text/css"/>
+          <link href="/favicon.ico" type="image/x-icon" rel="shortcut icon" />
+          <link href="/favicon.ico" type="image/x-icon" rel="icon" />
+
+          <link rel="stylesheet" href={url.resolve(assets, 'app.css')} />
+
+          <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic|Alegreya:400,400italic,700,700italic" rel="stylesheet" type="text/css" />
         </head>
         <body className={this.props.page + ' ' + this.props.theme}>
           <div className="app-container" id="app-mount"></div>
